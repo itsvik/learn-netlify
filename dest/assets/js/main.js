627e63388155f17308b15ca56d96bb921c1aceb6
vikramsrivastava <vikram@goodlpay.com>
2020-04-16 12:22:03 +0530

    Add bootstrap to look better

--- dest/assets/js/main.js
+++ dest/assets/js/main.js
@@ -9,11 +9,12 @@ getWord();
 
 
 const getUsers = async() => {
-    const res = await fetch('/getuser');
+    const res = await fetch('/api/users');
     const users = await res.json();
 
     users.forEach(user => {
         const li = document.createElement('li');
+        li.setAttribute("class", "list-group-item");
         const text = document.createTextNode(user.name);
         li.appendChild(text);
         document.getElementById('users').appendChild(li);
